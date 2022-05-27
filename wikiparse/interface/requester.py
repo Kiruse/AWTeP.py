3f7b37d29d78b638c97d21712fc907c702b0c9f5
Kiruse <26940742+Kiruse@users.noreply.github.com>
2022-05-27 18:54:52 +0200

    add return type information based on requests package

--- wikiparse/interface/requester.py
+++ wikiparse/interface/requester.py
@@ -1,25 +1,26 @@
 """Defines the interface we use for requesting HTTP CRUD operations.
 Note that every method should be asynchronous."""
 from __future__ import annotations
+from requests import Response
 from typing import *
 
 class Requester:
-  async def get(self, url: str, *args, params: Dict[str, Any], **kwargs):
+  async def get(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
     """Perform a `GET` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
   
-  async def post(self, url: str, *args, params: Dict[str, Any], **kwargs):
+  async def post(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
     """Perform a `POST` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
   
-  async def put(self, url: str, *args, params: Dict[str, Any], **kwargs):
+  async def put(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
     """Perform a `PUT` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
   
-  async def update(self, url: str, *args, params: Dict[str, Any], **kwargs):
+  async def update(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
     """Synonymous for `Requester.put`."""
     raise NotImplementedError()
   
-  async def delete(self, url: str, *args, params: Dict[str, Any], **kwargs):
+  async def delete(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
     """Perform a `DELETE` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
