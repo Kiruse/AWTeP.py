2893d5d042cfab90bd473b2b8fb2bfb158a3db32
Kiruse <26940742+Kiruse@users.noreply.github.com>
2022-05-27 18:55:35 +0200

    remove false double-await type info

--- wikiparse/interface/requester.py
+++ wikiparse/interface/requester.py
@@ -5,22 +5,22 @@ from requests import Response
 from typing import *
 
 class Requester:
-  async def get(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
+  async def get(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Response:
     """Perform a `GET` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
   
-  async def post(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
+  async def post(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Response:
     """Perform a `POST` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
   
-  async def put(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
+  async def put(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Response:
     """Perform a `PUT` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
   
-  async def update(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
+  async def update(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Response:
     """Synonymous for `Requester.put`."""
     raise NotImplementedError()
   
-  async def delete(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Awaitable[Response]:
+  async def delete(self, url: str, *args, params: Dict[str, Any], **kwargs) -> Response:
     """Perform a `DELETE` request to the given `url`. Underlying API should support `params`."""
     raise NotImplementedError()
