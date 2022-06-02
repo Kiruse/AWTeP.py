e4ee13db1378dc0a52207c1457851d8e1325fdea
Kiruse <26940742+Kiruse@users.noreply.github.com>
2022-06-02 02:20:59 +0200

    additional utility functions

--- wikiparse/utils.py
+++ wikiparse/utils.py
@@ -1,8 +1,26 @@
 """Some utility functions used across the library."""
 from __future__ import annotations
 from typing import *
+from .ast import AST, ASTList
 
 def first(it: Iterable[T]) -> T:
   return next(iter(it))
+
+def isiterable(it) -> bool:
+  try:
+    iter(it)
+    return True
+  except TypeError:
+    return False
+
+def find_nodes(pred: Callable[[AST], bool], ast: ASTList) -> List[AST]:
+  if type(ast) is not str and ast is not None:
+    for node in ast:
+      if type(node) == str:
+        continue
+      if isiterable(node):
+        yield from find_nodes(pred, node)
+      elif pred(node):
+        yield node
 
 T = TypeVar("T")
