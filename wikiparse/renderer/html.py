c616896b4aa0118bbe4995f77bdeba6ef031c411
kiruse <26940742+Kiruse@users.noreply.github.com>
2022-07-16 16:47:17 +0200

    add additional nodes to HTMLRenderer

--- wikiparse/renderer/html.py
+++ wikiparse/renderer/html.py
@@ -9,6 +9,22 @@ from .renderer import Renderer
 identifier_renderer = IdentifierRenderer()
 
 class HTMLRenderer(Renderer):
+  """The standard `HTMLRenderer` which can render most standard AST nodes (from the `wikiparse.ast` module).
+  Transclusion-specific nodes (templates, variables, parser functions, and magic words) are not rendered as they should
+  be transcluded before rendering anyways. The `DefRefNode`, which is a non-standard node in the WikiText specification
+  to be refactored in a future update, is also not handled by this renderer.
+  
+  **Note:** The `IndentNode` is currently rendered using MediaWiki standard *<dd>* tag. Subjectively, this is a
+  non-semantic use of the tag and should be overridden.
+  
+  **Caveat:** Multi-level lists are somewhat non-trivial to handle properly and depend partially on your specific
+  rendering needs. Normally, multi-level lists would be rendered by wrapping another *<ol>* or *<ul>* HTML tag in an
+  *<li>* tag, but this does not exactly align with the WikiText notation. A better representation would be to add left
+  visual padding to the list items. So instead of enforcing a standard solution here, this renderer produces *<li>* tags
+  following this pattern: `<li data-depth="{depth}">{content}</li>`. If indeed a nested representation is desired, it is
+  recommended to apply a custom `Transformer` which groups consecutive list items of the same depth into their own list
+  node, and perhaps override the `render_list` method.
+  """
   def __init__(self):
     super().__init__()
   
@@ -31,6 +47,26 @@ class HTMLRenderer(Renderer):
       return f'<{tag} {sattrs}>{self.render(content)}</{tag}>'
     else:
       return f'<{tag}>{self.render(content)}</{tag}>'
+  
+  def render_heading(self, h: HeadingNode):
+    cnt, lvl = h.children
+    return f'<h{lvl}>{self.render(cnt)}</h{lvl}>'
+  
+  def render_link(self, link: LinkNode):
+    label, url = link.children
+    return f'<a href="{htmlescape(url)}">{label}</a>'
+  
+  def render_linebreak(self, _):
+    return '<br/>'
+  
+  def render_list(self, list: ListNode):
+    ordered, items = list.children
+    tag = 'ol' if ordered else 'ul'
+    return f'<{tag}>{self.render(items)}</{tag}>'
+  
+  def render_listitem(self, item: ListItemNode):
+    depth, cnt = list.children
+    return f'<li data-depth="{depth}">{self.render(cnt)}</li>'
 
 def htmlescape(s: str) -> str:
   s = re.sub('"', '\\"', s)
